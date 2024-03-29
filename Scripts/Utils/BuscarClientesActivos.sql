789b86b612db11cfbb2c1b210e5825fc1a94736a
ANEUDY ABREU <aneudy91@gmail.com>
2024-03-29 03:47:35 -0600

    feat: agrega script de utilerias para encontrar clientes activos y con cuestas y tarjetas activas para probar

--- Scripts/Utils/BuscarClientesActivos.sql
+++ Scripts/Utils/BuscarClientesActivos.sql
@@ -1,1 +1,7 @@
 ﻿
+select *
+from [Finanzas].[tblCuentas] c
+	join [Clientes].[tblClientes] cli on cli.IDCliente = c.IDCliente
+	join [Finanzas].[tblTarjetasDebito] tb on tb.IDCuenta = c.IDCuenta
+	join [Catalogos].[tblEstatusCuentas] ec on ec.IDEstatusCuenta = c.IDEstatusCuenta
+where c.IDEstatusCuenta = 1 and tb.IDEstatusTarjeta = 1 and  cli.IDEstatusCliente = 1
