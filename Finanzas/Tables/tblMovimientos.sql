bbd9c247bd11db9bd76d5150283f05ab87c3abbf
ANEUDY ABREU <aneudy91@gmail.com>
2024-03-29 01:32:31 -0600

    fix: corrige el nombre de la tabla Finanzas.tblMovimientos, el nombre del campo IDMovimiento, los constriant y los triggers relacionados

--- Finanzas/Tables/tblMovimientos.sql
+++ Finanzas/Tables/tblMovimientos.sql
@@ -1,16 +1,16 @@
-﻿CREATE TABLE [Finanzas].[tblMovientos] (
-    [IDMoviento]        INT             IDENTITY (1, 1) NOT NULL,
+﻿CREATE TABLE [Finanzas].[tblMovimientos] (
+    [IDMovimiento]      INT             IDENTITY (1, 1) NOT NULL,
     [IDCuenta]          INT             NULL,
     [IDTarjetaDebito]   INT             NULL,
     [IDTarjetaCredito]  INT             NULL,
-    [Cargo]             DECIMAL (18, 2) CONSTRAINT [D_FinanzasTblMovientos_Cargo] DEFAULT ((0.00)) NOT NULL,
-    [Abono]             DECIMAL (18, 2) CONSTRAINT [D_FinanzasTblMovientos_Abono] DEFAULT ((0.00)) NOT NULL,
-    [SaldoActual]       DECIMAL (18, 2) CONSTRAINT [D_FinanzasTblMovientos_SaldoActual] DEFAULT ((0.00)) NOT NULL,
+    [Cargo]             DECIMAL (18, 2) CONSTRAINT [D_FinanzasTbMovimientos_Cargo] DEFAULT ((0.00)) NOT NULL,
+    [Abono]             DECIMAL (18, 2) CONSTRAINT [D_FinanzasTbMovimientos_Abono] DEFAULT ((0.00)) NOT NULL,
+    [SaldoActual]       DECIMAL (18, 2) CONSTRAINT [D_FinanzasTbMovimientos_SaldoActual] DEFAULT ((0.00)) NOT NULL,
     [FechaHoraRegistro] DATETIME        CONSTRAINT [D_FinanzasTblMovimientos_FechaHoraRegistro] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [Pk_FinanzasTblMovientos_IDMoviento] PRIMARY KEY CLUSTERED ([IDMoviento] ASC),
-    CONSTRAINT [Fk_FinanzasTblMovientos_FinanzasTblCuentas_IDCuenta] FOREIGN KEY ([IDCuenta]) REFERENCES [Finanzas].[tblCuentas] ([IDCuenta]),
-    CONSTRAINT [Fk_FinanzasTblMovientos_FinanzasTblTarjetasCredito_IDTarjetaCredito] FOREIGN KEY ([IDTarjetaCredito]) REFERENCES [Finanzas].[tblTarjetasCredito] ([IDTarjetaCredito]),
-    CONSTRAINT [Fk_FinanzasTblMovientos_FinanzasTblTarjetasDebito_IDTarjetaDebito] FOREIGN KEY ([IDTarjetaDebito]) REFERENCES [Finanzas].[tblTarjetasDebito] ([IDTarjetaDebito])
+    CONSTRAINT [Pk_FinanzasTblMovimientos_IDMovimiento] PRIMARY KEY CLUSTERED ([IDMovimiento] ASC),
+    CONSTRAINT [Fk_FinanzasTblMovimientos_FinanzasTblCuentas_IDCuenta] FOREIGN KEY ([IDCuenta]) REFERENCES [Finanzas].[tblCuentas] ([IDCuenta]),
+    CONSTRAINT [Fk_FinanzasTblMovimientos_FinanzasTblTarjetasCredito_IDTarjetaCredito] FOREIGN KEY ([IDTarjetaCredito]) REFERENCES [Finanzas].[tblTarjetasCredito] ([IDTarjetaCredito]),
+    CONSTRAINT [Fk_FinanzasTblMovimientos_FinanzasTblTarjetasDebito_IDTarjetaDebito] FOREIGN KEY ([IDTarjetaDebito]) REFERENCES [Finanzas].[tblTarjetasDebito] ([IDTarjetaDebito])
 );
 
 
@@ -18,7 +18,7 @@
 
 GO
 CREATE TRIGGER Finanzas.trActualizarCuentaMonto
-ON Finanzas.tblMovientos
+ON Finanzas.tblMovimientos
 AFTER INSERT
 AS
 BEGIN
@@ -55,7 +55,7 @@ END;
 GO
 
 CREATE TRIGGER Finanzas.trActualizarCreditoTarjeta
-ON Finanzas.tblMovientos
+ON Finanzas.tblMovimientos
 AFTER INSERT
 AS
 BEGIN
