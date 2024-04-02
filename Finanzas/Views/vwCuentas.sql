d3ed96422fbc3909ea3764049d7379d389b8ac88
ANEUDY ABREU <aneudy91@gmail.com>
2024-04-02 01:40:11 -0600

    feat: agrega nueva vista vwCuentas

---
+++ Finanzas/Views/vwCuentas.sql
@@ -0,0 +1,13 @@
+﻿create   view [Finanzas].[vwCuentas] as
+	select 
+		c.IDCuenta
+		,c.IDCliente
+		,cli.Cliente
+		,cli.[Estatus Cliente]
+		,c.NumeroCuenta as [Numero Cuenta]
+		,c.Saldo
+		,c.IDEstatusCuenta
+		,ec.Nombre as [Estatus Cuenta]
+	from [Finanzas].[tblCuentas] c
+		join [Clientes].[vwListadoClientes] cli on cli.IDCliente = c.IDCliente
+		join [Catalogos].[tblEstatusCuentas] ec on ec.IDEstatusCuenta = c.IDEstatusCuenta
