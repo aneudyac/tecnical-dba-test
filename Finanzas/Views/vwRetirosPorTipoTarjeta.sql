fc7229f9c794710839dca8f6a36dc23119ec3c50
ANEUDY ABREU <aneudy91@gmail.com>
2024-04-02 01:42:18 -0600

    feat: agrega nueva vista vwRetirosParTipoTarjeta

---
+++ Finanzas/Views/vwRetirosPorTipoTarjeta.sql
@@ -0,0 +1,36 @@
+﻿--create table [Catalogos].[tblTiposTarjetas](
+--	IDTipoTarjeta int not null constraint Pk_CatalogosTblTiposTarjetas_IDTarjeta primary key,
+--	Nombre varchar(20) not null
+----)
+
+
+--select * from [Finanzas].[vwRetirosPorTipoTarjeta] 
+--GO
+
+create   view [Finanzas].[vwRetirosPorTipoTarjeta] as
+	select 
+		c.IDCliente
+		,1 AS IDTipoTarjeta
+		,m.IDMovimiento
+		,m.IDTarjetaDebito as IDTarjeta
+		,t.NumeroTarjeta
+		,m.Cargo
+		,m.SaldoActual
+		,m.FechaHoraRegistro
+	from [Finanzas].[tblMovimientos] m
+		join [Finanzas].[tblTarjetasDebito] t on t.IDTarjetaDebito = m.IDTarjetaDebito
+		join [Finanzas].[tblCuentas] c on c.IDCuenta = t.IDCuenta
+	where m.IDCuenta is not null and m.IDTarjetaDebito is not null and isnull(m.Cargo, 0) > 0
+	UNION ALL
+	select 
+		t.IDCliente
+		,2 AS IDTipoTarjeta
+		,m.IDMovimiento
+		,m.IDTarjetaCredito as IDTarjeta
+		,t.NumeroTarjeta
+		,m.Cargo
+		,m.SaldoActual
+		,m.FechaHoraRegistro
+	from [Finanzas].[tblMovimientos] m
+		join [Finanzas].[tblTarjetasCredito] t on t.IDTarjetaCredito = m.IDTarjetaCredito
+	where m.IDTarjetaCredito is not null and isnull(m.Cargo, 0) > 0
