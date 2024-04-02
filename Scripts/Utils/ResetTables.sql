2676e20bed338f8684a4c821804989beed2f7632
ANEUDY ABREU <aneudy91@gmail.com>
2024-04-02 01:35:26 -0600

    feat: agrega clientes

--- Scripts/Utils/ResetTables.sql
+++ Scripts/Utils/ResetTables.sql
@@ -2,11 +2,11 @@
 delete [Finanzas].[tblTarjetasCredito]
 delete [Finanzas].[tblTarjetasDebito]
 delete [Finanzas].[tblCuentas]
---delete [Clientes].[tblClientes]
+delete [Clientes].[tblClientes]
 
 
 DBCC CHECKIDENT ('[Finanzas].[tblMovimientos]', RESEED, 0);
 DBCC CHECKIDENT ('[Finanzas].[tblTarjetasCredito]', RESEED, 0);
 DBCC CHECKIDENT ('[Finanzas].[tblTarjetasDebito]', RESEED, 0);
 DBCC CHECKIDENT ('[Finanzas].[tblCuentas]', RESEED, 0);
---DBCC CHECKIDENT ('[Clientes].[tblClientes]', RESEED, 0);
+DBCC CHECKIDENT ('[Clientes].[tblClientes]', RESEED, 0);
