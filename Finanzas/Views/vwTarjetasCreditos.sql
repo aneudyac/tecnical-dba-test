988a6bdd750f98185383c38e8b2e25dc0540967a
ANEUDY ABREU <aneudy91@gmail.com>
2024-04-02 01:43:07 -0600

    feat: agrega vista vwTarjetasCreditos

---
+++ Finanzas/Views/vwTarjetasCreditos.sql
@@ -0,0 +1,13 @@
+﻿create   view [Finanzas].[vwTarjetasCreditos] as
+	select 
+		 tc.IDTarjetaCredito
+		,tc.IDCliente
+		,tc.NumeroTarjeta
+		,tc.LimiteCredito
+		,tc.DiaDeCorte
+		,tc.IDEstatusTarjeta
+		,et.Nombre as [Estatus Tarjetas]
+		,tc.FechaHoraRegistro
+		,tc.Credito
+	from [Finanzas].[tblTarjetasCredito] tc
+		join [Catalogos].[tblEstatusTarjetas] et on et.IDEstatusTarjeta = tc.IDEstatusTarjeta
