e00b6917418f408be6338fb7e2f980350720e6a4
ANEUDY ABREU <aneudy91@gmail.com>
2024-04-02 01:41:17 -0600

    feat: agrega vistas MovimientosTarjetas Debitos y Creditos

---
+++ Finanzas/Views/vwMovimientosTarjetasCredito.sql
@@ -0,0 +1,12 @@
+﻿create   view [Finanzas].[vwMovimientosTarjetasCredito] as
+	select 
+		 m.IDMovimiento
+		,m.IDTarjetaCredito
+		,tb.NumeroTarjeta
+		,m.Cargo
+		,m.Abono
+		,m.SaldoActual
+		,m.FechaHoraRegistro
+	from [Finanzas].tblMovimientos m
+		join [Finanzas].[tblTarjetasCredito] tb on tb.IDTarjetaCredito = m.IDTarjetaCredito
+	where m.IDTarjetaCredito is not null
---
+++ Finanzas/Views/vwMovimientosTarjetasDebito.sql
@@ -0,0 +1,13 @@
+﻿create   view [Finanzas].[vwMovimientosTarjetasDebito] as
+	select 
+		 m.IDMovimiento
+		,m.IDCuenta
+		,m.IDTarjetaDebito
+		,tb.NumeroTarjeta
+		,m.Cargo
+		,m.Abono
+		,m.SaldoActual
+		,m.FechaHoraRegistro
+	from [Finanzas].tblMovimientos m
+		join [Finanzas].[tblTarjetasDebito] tb on tb.IDTarjetaDebito = m.IDTarjetaDebito
+	where m.IDCuenta is not null and m.IDTarjetaDebito is not null
