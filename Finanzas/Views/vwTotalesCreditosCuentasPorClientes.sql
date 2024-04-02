6a38e1548ee7da20054c207c2d128a0a39f0e2a6
ANEUDY ABREU <aneudy91@gmail.com>
2024-04-02 01:45:20 -0600

    feat: agrega vista vwTotalesCreditosCuentasPorClientes

---
+++ Finanzas/Views/vwTotalesCreditosCuentasPorClientes.sql
@@ -0,0 +1,19 @@
+﻿
+create view [Finanzas].[vwTotalesCreditosCuentasPorClientes] as
+
+--declare 
+--	@ID_ESTATUS_CLIENTE_ACTIVO int = 1,
+--	@ID_ESTATUS_CUENTA_ACTIVA int = 1,
+--	@ID_ESTATUS_TARJETA_ACTIVA int = 1
+--;
+
+
+select 
+	c.IDCliente,
+	sum(tc.Credito) as TotalCredito,
+	sum(cu.Saldo) as TotalCuentas
+from [Clientes].tblClientes c
+	join [Finanzas].[tblTarjetasCredito] tc on tc.IDCliente = c.IDCliente and tc.IDEstatusTarjeta = 1
+	join [Finanzas].[tblCuentas] cu on c.IDCliente = cu.IDCliente and cu.IDEstatusCuenta = 1
+where c.IDEstatusCliente = 1
+group by c.IDCliente
